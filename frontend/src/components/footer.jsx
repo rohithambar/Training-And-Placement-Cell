799733fbc90c71b2e611274c3ba48164a3abc9cf
Sagar Soradi <sagarsordi011@gmail.com>
2025-06-22 21:02:43 +0530

    fixed initial issues

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/footer.css';  // Add this import
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>©Copyrights- KLE Technological University. All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
